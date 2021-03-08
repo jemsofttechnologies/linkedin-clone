72d5f02dd2ff66014a7b27f90128fafb25a6b41a
jemsofttechnologies <jemsofttechnologies@gmail.com>
2021-03-08 15:01:46 +0300

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZdCZu-6J1aUdOtnF43bZyXQ0WhSxgXyo",
+  authDomain: "linkedin-clone-add09.firebaseapp.com",
+  projectId: "linkedin-clone-add09",
+  storageBucket: "linkedin-clone-add09.appspot.com",
+  messagingSenderId: "718849398325",
+  appId: "1:718849398325:web:fbbe0304d5108fab06037c",
+};
